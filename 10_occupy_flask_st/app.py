9ee5ab17a46ce633182148819a6f0a480a0b15dc
Claire Liu <cliu5@stuy.edu>
2018-09-23 21:48:22 -0400

    Updated with comments

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,26 +1,37 @@
+#Team Cucumber
+#Claire Liu + Kaitlin Wan
+#SoftDev1 PD6
+#K #10: Jinja Tuning ...
+#2018-09-24
+
 from flask import Flask, render_template
 from random import choice
-app=Flask(__name__)
+app=Flask(_name_)
+
+#From Claire Liu's HW 2:
+#Uses more robust code + is easier to follow
 def dictionaryMaker():
     dictionary = {}
     file = open('data/occupations.csv','r')
     straw = file.read()
-    lines = straw.split("\n")
+    lines = straw.split("\n") #seperates file
     #delete useless lines
     del lines[0]
     del lines[-1]
     del lines[-1]
+
     for line in lines:
         #sets comma to last comma (only relevant)
         comma=line.rfind(",")
         #before comma = key, after = val
-        key =line[:comma]
+        key = line[:comma]
         val = line[comma+1:]
-        dictionary[key] = val
+        dictionary[key] = val #creates new key for each value
     return dictionary
 
+#randomly selected occupation shown at the top
 def randomJob():
-    jobs = dictionaryMaker()
+    jobs = dictionaryMaker() #job is set to dictinary of occupations
     #weightedJobs will store reps of jobs
     weightedJobs = []
     for jobKey in jobs:
@@ -36,8 +47,9 @@ def helloWorld():
 
 @app.route("/occupations")
 def occupations():
+    #uses our template in /templates directory
     return render_template('template.html',randomJob=randomJob(),jobs=dictionaryMaker())
 
-if (__name__ == "__main__"):
+if (_name_ == "_main_"):
     app.debug = True
     app.run()
