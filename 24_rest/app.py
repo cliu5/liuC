ad81c7c239c73db3fa81c20117d8803cfc6c07ac
Claire Liu <cliu5@stuy.edu>
2018-11-13 22:26:08 -0500

    Create app.py

---
+++ 24_rest/app.py
@@ -0,0 +1,26 @@
+#Claire Liu + Addison Huang
+#SoftDev1 PD6
+#K24: A RESTful Journey Skyward
+#2018-11-13  
+
+from flask import Flask, render_template
+import urllib, json
+
+app=Flask(__name__)
+
+@app.route("/")
+def root():
+    urlData = "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"
+    webURL = urllib.request.urlopen(urlData)
+    data = webURL.read()
+    encoding = webURL.info().get_content_charset('utf-8')
+    dic = json.loads(data.decode(encoding))
+    print(dic)
+    print (dic['url'])
+    return render_template(pic = dic['url'], "index.html")
+    
+    
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
