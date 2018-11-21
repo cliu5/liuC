7327b603d7bf38c756098eb57a3f570f3ec3301c
cliu5 <cliu5@stuy.edu>
2018-11-20 23:02:06 -0500

    woo

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -63,7 +63,7 @@ def advice():
 
 
 @app.route("/google")
-def advice():
+def google():
 #https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
     
     urlData="https://www.googleapis.com/customsearch/v1?key="
