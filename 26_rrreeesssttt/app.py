e40840ef595d6eb7774a9dcfbadb6110c0f1605c
cliu5 <cliu5@stuy.edu>
2018-11-20 23:00:36 -0500

    some google stuff

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -12,7 +12,7 @@ import random
 context = ssl._create_unverified_context()
 
 app=Flask(__name__)
-
+'''
 @app.route("/")
 def met():
     urlData = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
@@ -60,7 +60,26 @@ def advice():
     advice=data['slip']['advice']
     slip=data['slip']['slip_id']
     return render_template("advice.html",_advice=advice,_slip=slip)
+'''
+
+@app.route("/google")
+def advice():
+#https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
     
+    urlData="https://www.googleapis.com/customsearch/v1?key="
+    key="AIzaSyDLFqAoBs-xQCm9XPVAlTsTa0jG8ewM57k"
+    urlData2="&cx=009364855531151632334:atzshazndou&q=dog"
+    webURL=urllib.request.urlopen(urlData+key+urlData2,context=context)
+    data=webURL.read()
+    data=json.loads(data)
+    print("------------")
+    #print(data)
+    print(data['items'][0]['title'])
+    print("-----------")
+    title=data['items'][0]['title']
+    return render_template("google.html",_title=title)
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
